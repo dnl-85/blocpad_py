7b76b7bfbe1ae06c23a2a26ee5d1898ad7cd2a36
Daniel <105582911+dnl-85@users.noreply.github.com>
2022-05-16 22:21:54 +0200

    Add files via upload

--- blocpad.py
+++ blocpad.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
-import os, sys
-from datetime import datetime
+import cython
+import os, sys, getpass
+from datetime import date, datetime
 
-os.system("clear")
-print(f" > BLOCPAD_PY <")
+def main(option):
+    if "-r" in option: lire()
+    elif "-w" in option: ajout()
+    else: print ("... option(s) inconnue(s)...")
+    exit()
 
-if "-r" in sys.argv:
+def lire():
     with open("blocpad.txt", "r") as fichier:
         contenu = fichier.read()
     print (f"\n{contenu}\n")
-elif "-w" in sys.argv:
+    exit()
+
+def ajout():
     message = input(" > ")
-    message = f"{str(datetime.now())}  : {message} \n"
+    message = f"{horodatage()}  : {message} \n"
     with open("blocpad.txt", "a") as fichier:
         fichier.write(message)
-else:
-    print ("... option(s) inconnue(s)...")
-    
+    exit()
+       
+def horodatage():
+    jour = date.today()
+    heure = datetime.now()
+    login = getpass.getuser()
+    moment = login + " - " + jour.strftime("%d/%m/%Y") + " - " + heure.strftime("%H:%M:%S")
+    return moment
+
+main(sys.argv)
